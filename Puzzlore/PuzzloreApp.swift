e449943359ad7b20b760b505a6aa9c7d33e16dab
ndmlabs <ndmlaboratories@gmail.com>
2025-11-27 13:25:14 -0800

    Initial Commit

---
+++ Puzzlore/PuzzloreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzloreApp.swift
+//  Puzzlore
+//
+//  Created by Daniel Merryman on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PuzzloreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
